a90ec1f302e12f9be03fd1a58723e383d96588ec
Dawid <daw.parc@gmail.com>
2022-11-15 18:32:06 +0100

    Remove contents of titleList

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-/*document.getElementById('test-button').addEventListener('click', function(){
-        const links = document.querySelectorAll('.titles a');
-        console.log('links:', links);
-    });
-*/
-
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -40,7 +34,7 @@ const titleClickHandler = function(event){
     const targetArticle = document.querySelector(articleSelektor);
     console.log(targetArticle);
 
-    /* [IN PROGRESS] add class 'active' to the correct article */ 
+    /* [DONE] add class 'active' to the correct article */ 
 
     targetArticle.classList.add('active');
 
@@ -50,4 +44,33 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
+
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+  
+  function generateTitleLinks(){
+  
+    /* remove contents of titleList */
+    
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+  
+      /* get the article id */
+  
+      /* find the title element */
+  
+      /* get the title from the title element */
+  
+      /* create HTML of the link */
+  
+      /* insert link into titleList */
+  
+  }
+  
+  generateTitleLinks();
+
+
 }
